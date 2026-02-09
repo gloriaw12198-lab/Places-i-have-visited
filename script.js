5fc533a4bc6b835baaf80f840737569186dd9fd2
Gloria <gloriaw12198@gmail.com>
2026-02-09 20:35:15 +0300

    did some changes added images changed some style

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
-function Place(location, landmarks, timeOfYear, notes) {
+function Place(location, landmarks, timeOfYear, notes, image) {
   this.location = location;
   this.landmarks = landmarks;
   this.timeOfYear = timeOfYear;
   this.notes = notes;
+  this.image = image;
 }
 
 Place.prototype.getLandmarks = function () {
@@ -16,42 +16,54 @@ const places = [
     "Mombasa",
     ["Indian Ocean", "Fort Jesus"],
     "December",
-    "Hot and relaxing. The beach and waves"
+    "Hot and relaxing. The beach and waves",
+    "https://images.unsplash.com/photo-1623317977555-5be922ef3f5f?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   ),
   new Place(
     "Zanzibar",
     ["Stone Town", "Beaches"],
     "August",
-    "Beautiful island vibes"
+    "Beautiful island vibes",
+    "https://images.unsplash.com/photo-1711091704998-3bea86948c83?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8emFuemliYXIlMjBpc2xhbmR8ZW58MHx8MHx8fDA%3D"
   ),
   new Place(
     "Maasai Mara",
     ["Wildlife", "Great Migration"],
     "July",
-    "Amazing safari experience. The hot air balloon experience"
+    "Amazing safari experience. Hot air balloon ride",
+    "https://images.unsplash.com/photo-1519659528534-7fd733a832a0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFhc2FpJTIwbWFyYXxlbnwwfHwwfHx8MA%3D%3D",
+    
   ),
   new Place(
     "Tsavo",
-    ["Red elephants", "National Park"],
+    ["Red Elephants", "National Park"],
     "June",
-    "Very scenic and peaceful. The amazing people"
+    "Very scenic and peaceful",
+    "https://images.unsplash.com/photo-1732548314474-2aff3b4e8e55?q=80&w=902&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
   )
 ];
 
 // UI LOGIC
 const placesList = document.getElementById("places-list");
 
+function showPlace(place) {
+  document.getElementById("detail-location").textContent = place.location;
+  document.getElementById("detail-landmarks").textContent = place.getLandmarks();
+  document.getElementById("detail-time").textContent = place.timeOfYear;
+  document.getElementById("detail-notes").textContent = place.notes;
+  document.getElementById("detail-image").src = place.image;
+}
+
+// Default view
+showPlace(places[0]);
+
 places.forEach(function (place) {
   const li = document.createElement("li");
   li.textContent = place.location;
 
   li.addEventListener("click", function () {
-    document.getElementById("detail-location").textContent = place.location;
-    document.getElementById("detail-landmarks").textContent = place.getLandmarks();
-    document.getElementById("detail-time").textContent = place.timeOfYear;
-    document.getElementById("detail-notes").textContent = place.notes;
+    showPlace(place);
   });
 
   placesList.appendChild(li);
 });
-
